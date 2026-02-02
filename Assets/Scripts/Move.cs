0c73ac04f56f4f8285eb492bc7400f568e8d1f1c
LAPTOP-6EK9OKR7\Owner <brie.noellee@gmail.com>
2026-02-02 10:01:30 -0500

    got tanks to spawn at different speeds

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    public float speed = 1f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,14 @@ public class Move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.right * Time.deltaTime;
+        transform.position += transform.right * speed * Time.deltaTime;
+
+        Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (screenPos.x < 0 || screenPos.x > Screen.width)
+        {
+            speed = speed * -1;
+
+        }
     }
 }
--- Assets/Scripts/TankSpawnerr.cs
+++ Assets/Scripts/TankSpawnerr.cs
@@ -4,6 +4,12 @@ using UnityEngine.InputSystem;
 public class TankSpawnerr : MonoBehaviour
 {
     public GameObject tankPrefab; // Reference to the tank prefab
+    public GameObject spawnedTank; // 
+
+    public int howManyTanks = 0;
+
+    public Move tankScript;
+    public SpriteRenderer tankSR;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,7 +22,27 @@ public class TankSpawnerr : MonoBehaviour
     {
         if (Mouse.current.leftButton.wasPressedThisFrame)
         {
-            Instantiate(tankPrefab);
+            //Instantiate a prefab: make this one appear
+            //Instantiate a prefab, a Vector2 or Vector3, a quaternion: make it appear at this position and rotation
+            //Instantiate(tankPrefab);
+            //Instantiate(tankPrefab, transform.position, transform.rotation);
+
+            Vector2 spawnPosition = Random.insideUnitCircle * 3f;
+
+            //Quaternion.identity means "no rotation" which is the same as Euler angles (0,0,0)
+            spawnedTank = Instantiate(tankPrefab, spawnPosition, Quaternion.identity);
+            tankSR = spawnedTank.GetComponent<SpriteRenderer>();
+
+            tankScript = spawnedTank.GetComponent<Move>();
+
+            howManyTanks++;
+            tankScript.speed = howManyTanks;
         }
+        /*
+        if (Mouse.current.rightButton.wasPressedThisFrame)
+        {
+            //instantiate a Transform: make it appear as a child of this transform
+            Instantiate(tankPrefab, transform);
+        }*/
     }
 }
